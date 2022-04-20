91d3fce948f57c3c63a3e19e080f11270ac5986c
Garima1007 <sharma.garima1007@gmail.com>
2022-04-20 13:00:07 +0530

    First Java Commit

---
+++ JavaProject/src/BasicJavaProgram/Class1.java
@@ -0,0 +1,11 @@
+package BasicJavaProgram;
+
+public class Class1 {
+
+	public static void main() {
+		System.out.println("Hi");
+		System.out.println("This is is my first Java commit");
+		
+	}
+	
+}
