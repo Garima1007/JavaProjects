10a8b4606d3775db155788fe1584cd884601b5a4
Garima1007 <sharma.garima1007@gmail.com>
2022-04-20 13:21:08 +0530

    BugFix

--- JavaProject/src/BasicJavaProgram/Class1.java
+++ JavaProject/src/BasicJavaProgram/Class1.java
@@ -2,10 +2,10 @@ package BasicJavaProgram;
 
 public class Class1 {
 
-	public static void main() {
+	public static void main(String[] args) {
+
 		System.out.println("Hi");
-		System.out.println("This is is my first Java commit");
-		
+		System.out.println("This is my first java commit");
 	}
-	
+
 }
